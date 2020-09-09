5135b9b187d063b200c5a3ec73303f518f24b5f8
Roman Mamchur <r_mamchur@ukr.net>
2020-09-09 20:30:37 +0300

    test

--- docker-proxy/entrypoint.sh
+++ docker-proxy/entrypoint.sh
@@ -8,9 +8,9 @@
  sed -i -e "s|https://dtapi.if.ua/api/|$URL_BE/|g" /var/www/dtapi/main-es2015.js.map
  sed -i -e "s|https://dtapi.if.ua/api/|$URL_BE/|g" /var/www/dtapi/main-es5.js /var/www/dtapi/main-es5.js.map
 
-echo '###'  /var/www/dtapi/api/application/config/database.php
-cat  /var/www/dtapi/api/application/config/database.php
+# echo '###'  /var/www/dtapi/api/application/config/database.php
+# cat  /var/www/dtapi/api/application/config/database.php
 
-cloud_sql_proxy -$INSTANCE_CONNECTION_NAME=tcp:3306 &
+cloud_sql_proxy -instances=$INSTANCE_CONNECTION_NAME=tcp:3306 &
   
 exec "$@"
